8a8808322555b386ac6e6756916885ca8793d96d
vaibhav shah <vaibhavshah96@gmail.com>
2020-11-30 22:30:25 -0500

    reverted changes to app

--- src/App.js
+++ src/App.js
@@ -4,7 +4,21 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <p>Vaibhav Shah</p>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>HELLO</code> and save to reload.
+        </p>
+
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
