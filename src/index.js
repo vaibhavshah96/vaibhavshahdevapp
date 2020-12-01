a6ec0bada36ae60ed207ca7969c0dc417d67b553
vaibhav shah <vaibhavshah96@gmail.com>
2020-11-30 22:32:59 -0500

    invalid imports removed

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import Home from "../../../workspace-spring-tool-suite-4-4.8.1.RELEASE/app/src/Home";
 
 ReactDOM.render(
   <React.StrictMode>
