223c4e0dbb3d9659f825dd7b0823d7b004cdac4e
vaibhav shah <vaibhavshah96@gmail.com>
2020-11-30 22:01:38 -0500

    removed button

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import logo from "./logo.svg";
-import Button from "react-bootstrap/Button";
 import "./App.css";
 
 function App() {
@@ -10,7 +9,6 @@ function App() {
         <p>
           Edit <code>HELLO</code> and save to reload.
         </p>
-        <Button variant="outline-dark">Dark</Button>
 
         <a
           className="App-link"
