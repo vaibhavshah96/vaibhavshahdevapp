b2806525f7ee7ddc6aae1b54b122feeeff438d4d
vaibhav shah <vaibhavshah96@gmail.com>
2020-12-02 13:54:33 -0500

    Adjusted Hero.

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -8,10 +8,10 @@ function Footer() {
     <footer className="mt-5">
       <Container fluid={true}>
         <Row className="border-top justify-content-between p-3">
-          <Col className="p-0" md={3}>
+          <Col className="p-0" md={3} sm={5}>
             Vaibhav Shah
           </Col>
-          <Col className="p-0 d-flex justify-content-end" md={2} sm={1}>
+          <Col className="p-0 d-flex justify-content-end" md={3} sm={12}>
             Made with React and AWS Amplify.
           </Col>
         </Row>
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -6,13 +6,19 @@ import Col from "react-bootstrap/Col";
 
 function Hero(props) {
   return (
-    <Jumbotron>
-      <Container>
-        <Row>
-          <Col>
-            {props.title && <h1>{props.title}</h1>}
-            {props.subTitle && <h3>{props.subTitle}</h3>}
-            {props.text && <h3>{props.text}</h3>}
+    <Jumbotron className="bg-transparent jumbotron-fluid p-0">
+      <Container fluid={true}>
+        <Row className="justify-content-center py-5">
+          <Col md={8} sm={12}>
+            {props.title && (
+              <h1 className="display-1 font-weight-bolder">{props.title}</h1>
+            )}
+            {props.subTitle && (
+              <h3 clasName="display-4 font-weight-light">{props.subTitle}</h3>
+            )}
+            {props.text && (
+              <h3 className="lead font-weight-light">{props.text}</h3>
+            )}
           </Col>
         </Row>
       </Container>
